0925c28ebfc13e8d844fa13120daca6eb2228561
Ali Altan Omurveren <altan.omurveren@outlook.com>
2023-11-24 00:43:51 +0300

    added core gameplay loop

--- Assets/Scripts/GameStateMachine/WinState.cs
+++ Assets/Scripts/GameStateMachine/WinState.cs
@@ -6,14 +6,34 @@ public class WinState : MonoBehaviour, IGameState
 {
 
     GameStateMachine gameStateMachine;
+    public RightArm rightArm;
     public Transform customerSpawn;
     public Transform customerTargetPosition;
     GameObject customer;
+    GameObject product;
 
     public void Enter()
     {
         customer = GameObject.Find("Customer");
         gameStateMachine = GameObject.Find("Game State Machine").GetComponent<GameStateMachine>();
+        product = GameObject.Find("Product");
+
+        StartCoroutine(MoveCustomerBack(3));
+
+        GameObject[] weightsToDestroy = GameObject.FindGameObjectsWithTag("Weight");
+
+        if(weightsToDestroy != null){
+            foreach (GameObject weight in weightsToDestroy){
+                Destroy(weight);
+            }
+
+            rightArm.totalWeightContributers = new List<GameObject>();
+            rightArm.bottomTouchingWeights = new List<GameObject>();
+        }
+
+        if(product != null){
+            Destroy(product);
+        }
     }
 
     public void Exit()
@@ -21,13 +41,15 @@ public class WinState : MonoBehaviour, IGameState
         
     }
 
-    void IGameState.Update()
+    public void Update()
     {
         
     }
 
-    IEnumerator MoveCustomerX(float seconds){
+    IEnumerator MoveCustomerBack(float seconds){
         float t = 0;
+
+        customer.GetComponent<SpriteRenderer>().flipX = true;
 
         while(t < 1){
             t += Time.deltaTime / seconds;
@@ -38,5 +60,7 @@ public class WinState : MonoBehaviour, IGameState
 
             yield return null;
         }
+
+        gameStateMachine.TransitionTo(gameStateMachine.customerEntranceState);
     }
 }
